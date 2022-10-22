14f773b35e3a75b133a1e90a71123afdc73fac17
Alena <alenaalenasmirnova86@gmail.com>
2022-10-22 17:31:10 +0300

    fifth commit

--- app/src/main/java/ru/smirnova/new2/MainActivity.java
+++ app/src/main/java/ru/smirnova/new2/MainActivity.java
@@ -10,6 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        System.out.println("hello world");
+        System.out.println("ohhhhh world");
     }
 }
